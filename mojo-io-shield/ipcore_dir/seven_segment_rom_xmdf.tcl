17e68d61431f73153408a4009207ede760baddcb
Eric Roch <eric@roch.us>
2018-05-31 00:42:44 -0400

    added 7 segment ROM for testing

---
+++ mojo-io-shield/ipcore_dir/seven_segment_rom_xmdf.tcl
@@ -0,0 +1,87 @@
+# The package naming convention is <core_name>_xmdf
+package provide seven_segment_rom_xmdf 1.0
+
+# This includes some utilities that support common XMDF operations
+package require utilities_xmdf
+
+# Define a namespace for this package. The name of the name space
+# is <core_name>_xmdf
+namespace eval ::seven_segment_rom_xmdf {
+# Use this to define any statics
+}
+
+# Function called by client to rebuild the params and port arrays
+# Optional when the use context does not require the param or ports
+# arrays to be available.
+proc ::seven_segment_rom_xmdf::xmdfInit { instance } {
+# Variable containing name of library into which module is compiled
+# Recommendation: <module_name>
+# Required
+utilities_xmdf::xmdfSetData $instance Module Attributes Name seven_segment_rom
+}
+# ::seven_segment_rom_xmdf::xmdfInit
+
+# Function called by client to fill in all the xmdf* data variables
+# based on the current settings of the parameters
+proc ::seven_segment_rom_xmdf::xmdfApplyParams { instance } {
+
+set fcount 0
+# Array containing libraries that are assumed to exist
+# Examples include unisim and xilinxcorelib
+# Optional
+# In this example, we assume that the unisim library will
+# be available to the simulation and synthesis tool
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type logical_library
+utilities_xmdf::xmdfSetData $instance FileSet $fcount logical_library unisim
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom/blk_mem_gen_v7_3_readme.txt
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom/doc/blk_mem_gen_v7_3_vinfo.html
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom/doc/pg058-blk-mem-gen.pdf
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom.asy
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type asy
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom.mif
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom.ngc
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom.veo
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog_template
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom.xco
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type coregen_ip
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path seven_segment_rom_xmdf.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path summary.log
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount associated_module seven_segment_rom
+incr fcount
+
+}
+
+# ::gen_comp_name_xmdf::xmdfApplyParams
---
+++ mojo-io-shield/ipcore_dir/tmp/_cg/selectio_if_wiz_v4_1.veo
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018 Xilinx, Inc.
+// All Rights Reserved
+///////////////////////////////////////////////////////////////////////////////
+//   ____  ____
+//  /   /\/   /
+// /___/  \  /    Vendor     : Xilinx
+// \   \   \/     Version    : 14.7
+//  \   \         Application: Xilinx CORE Generator
+//  /   /         Filename   : selectio_if_wiz_v4_1.veo
+// /___/   /\     Timestamp  : Wed May 30 16:30:40 EDT 2018
+// \   \  /  \
+//  \___\/\___\
+//
+// Design Name: ISE Instantiation template
+///////////////////////////////////////////////////////////////////////////////
+
+// The following must be inserted into your Verilog file for this
+// core to be instantiated. Change the instance name and port connections
+// (in parentheses) to your own signal names.
+
+//----------- Begin Cut here for INSTANTIATION Template ---// INST_TAG
+selectio_if_wiz_v4_1 YourInstanceName (
+    .DATA_IN_FROM_PINS(DATA_IN_FROM_PINS), // IN BUS [0:0]
+    .DATA_IN_FROM_PINS_P(DATA_IN_FROM_PINS_P), // IN BUS [0:0]
+    .DATA_IN_FROM_PINS_N(DATA_IN_FROM_PINS_N), // IN BUS [0:0]
+    .DATA_OUT_TO_PINS(DATA_OUT_TO_PINS), // OUT BUS [0:0]
+    .DATA_OUT_TO_PINS_P(DATA_OUT_TO_PINS_P), // OUT BUS [0:0]
+    .DATA_OUT_TO_PINS_N(DATA_OUT_TO_PINS_N), // OUT BUS [0:0]
+    .DATA_TO_AND_FROM_PINS(DATA_TO_AND_FROM_PINS), // INOUT BUS [0:0]
+    .DATA_TO_AND_FROM_PINS_P(DATA_TO_AND_FROM_PINS_P), // INOUT BUS [0:0]
+    .DATA_TO_AND_FROM_PINS_N(DATA_TO_AND_FROM_PINS_N), // INOUT BUS [0:0]
+    .CLK_IN(CLK_IN), // IN
+    .CLK_IN_P(CLK_IN_P), // IN
+    .CLK_IN_N(CLK_IN_N), // IN
+    .CLK_DIV_IN(CLK_DIV_IN), // IN
+    .CLK_DIV_OUT(CLK_DIV_OUT), // OUT
+    .LOCKED_IN(LOCKED_IN), // OUT
+    .LOCKED_OUT(LOCKED_OUT), // OUT
+    .DATA_IN_TO_DEVICE(DATA_IN_TO_DEVICE), // OUT BUS [0:0]
+    .DATA_OUT_FROM_DEVICE(DATA_OUT_FROM_DEVICE), // IN BUS [0:0]
+    .CLK_RESET(CLK_RESET), // IN
+    .IO_RESET(IO_RESET), // IN
+    .SYNC_RESET(SYNC_RESET), // IN
+    .DELAY_BUSY(DELAY_BUSY), // IN
+    .DELAY_CLK(DELAY_CLK), // IN
+    .DELAY_CLOCK_CAL(DELAY_CLOCK_CAL), // IN
+    .DELAY_CLOCK_CE(DELAY_CLOCK_CE), // IN
+    .DELAY_CLOCK_INC(DELAY_CLOCK_INC), // IN
+    .DELAY_DATA_CAL(DELAY_DATA_CAL), // IN
+    .DELAY_DATA_CE(DELAY_DATA_CE), // IN
+    .DELAY_DATA_INC(DELAY_DATA_INC), // IN
+    .BITSLIP(BITSLIP), // IN
+    .TRAIN(TRAIN), // IN
+    .TRISTATE_OUTPUT(TRISTATE_OUTPUT), // IN
+    .PHASE_SHIFTOUT(PHASE_SHIFTOUT), // IN
+    .PHASE_INCDEC(PHASE_INCDEC), // IN
+    .PHASE_VALID(PHASE_VALID) // IN
+);
+
+// INST_TAG_END ------ End INSTANTIATION Template ---------
+
